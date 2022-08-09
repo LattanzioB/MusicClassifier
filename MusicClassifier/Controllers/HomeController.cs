ea60d410e8f405f424b1c4cc148e7e1836ee18aa
lattanziob <brunoj.lattanzio@gmail.com>
2022-08-09 20:06:58 -0300

    init

---
+++ MusicClassifier/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace MusicClassifier.Controllers
+{
+    [ApiController]
+    [Route("api/home")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public string index()
+        {
+            return "Hello World";
+        }
+    }
+}
